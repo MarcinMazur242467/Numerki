382730bb15f1507e1680c01c1889641d9561176e
PiotrPleska <piotrekeska22@gmail.com>
2023-03-25 22:18:55 +0100

    Unsigned and contradictory equations

--- Zad2/main.py
+++ Zad2/main.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 
 
@@ -5,28 +6,49 @@ def jordanGaus(matrix, epsilon):
     size = len(matrix)
     vector = np.zeros(size)
     squareMatrix = matrix[:, :-1]
-    if abs(np.linalg.det(squareMatrix) - 0.0) < epsilon:
-        return "Nieoznaczony lub sprzeczny"
+    # if abs(np.linalg.det(squareMatrix) - 0.0) < epsilon:
+    #     return "Nieoznaczony lub sprzeczny"
     # Sprowadzamy do postaci schodkowej dolnej
     for i in range(0, size - 1):
         if abs(matrix[i, i] - 0.0) < epsilon:
-            return "FAILURE"
+            # Dodajemy do siebie wiersze, zeby uniknac zer w dzielniku
+            for z in range(0, size + 1):
+                if i == size:
+                    matrix[i][z] += matrix[0][z]
+                else:
+                    matrix[i][z] += matrix[i + 1][z]
+        # Sprawdzamy czy po dodaniu do siebie wierszy nie ma zadnych nanow (wtedy nieoznaczony)
+        for j in range(0, size):
+            for k in range(0, size + 1):
+                if math.isnan(matrix[i][j]):
+                    return "Układ równań jest nieoznaczony!"
         for j in range(i + 1, size):
             m = -(matrix[j, i] / matrix[i, i])
             for k in range(i, size + 1):
                 matrix[j, k] += m * matrix[i, k]
-    print(matrix)
     # Sprowadzamy do macierzy jednostkowej
     for i in range(size - 1, 0, -1):
         if abs(matrix[i, i] - 0.0) < epsilon:
-            return "FAILURE"
+            # Dodajemy do siebie wiersze, zeby uniknac zer w dzielniku
+            for z in range(size - 1, 0):
+                if i == size - 1:
+                    matrix[i][z] += matrix[0][z]
+                else:
+                    matrix[i][z] += matrix[i + 1][z]
+        # Sprawdzamy czy po dodaniu do siebie wierszy nie ma zadnych nanow (wtedy nieoznaczony)
+        for j in range(0, size):
+            for k in range(0, size + 1):
+                if math.isnan(matrix[i][j]):
+                    return "Układ równań jest nieoznaczony!"
         for j in range(i - 1, -1, -1):
             m = -(matrix[j, i] / matrix[i, i])
             for k in range(size, i - 1, -1):
                 matrix[j, k] += m * matrix[i, k]
-    print(matrix)
+    # Sprawdzamy wyznacznik
+    if abs(np.linalg.det(squareMatrix) - 0.0) < epsilon:
+        return "Układ równan jest sprzeczny!"
     for i in range(0, size):
-        if matrix[i][i]:
+        if matrix[i][i] == 0:
             vector[i] = 0
         else:
             vector[i] = matrix[i, size] / matrix[i][i]
@@ -34,6 +56,7 @@ def jordanGaus(matrix, epsilon):
 
 
 def main():
+    np.seterr(divide='ignore', invalid='ignore')
     size = 0
     with open("matrix.txt", 'r') as file:
         lines = file.readlines()
@@ -49,4 +72,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main();
+    main()
