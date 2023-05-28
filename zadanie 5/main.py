249b7812f9a99c667f1ac7f1780f956c6950e869
Marcin Mazur <242467@edu.p.lodz.pl>
2023-05-28 11:17:43 +0200

    done

--- zadanie 5/main.py
+++ zadanie 5/main.py
@@ -9,6 +9,7 @@ approximationRange = [0, 0]
 degree = 0
 function = None
 epsilon = 0.01
+nodes=0
 
 
 class GUI:
@@ -47,24 +48,13 @@ class GUI:
         self.button7 = tk.Button(self.root, text="Wprowadź stopień", command=self.getDegree)
         self.button7.pack()
 
-        self.label = tk.Label(self.root, text="Wprowadź przedział całkowania dla wzoru Simpsona: ", font=('Arial', 18))
+        self.label = tk.Label(self.root, text="Wprowadź ilosc wezlow podczas calkowania", font=('Arial', 18))
         self.label.pack(pady=10)
+        self.text4 = tk.Text(self.root, height=2, font=('Arial', 12))
+        self.text4.pack()
+        self.button5 = tk.Button(self.root, text="Wprowadz wezly", command=self.getNodes)
+        self.button5.pack()
 
-        self.text3 = tk.Text(self.root, height=1, font=('Arial', 12))
-        self.text3.pack()
-
-        self.button4 = tk.Button(self.root, text="Wprowadź przedział", command=self.getRange)
-        self.button4.pack()
-
-        # self.label = tk.Label(self.root, text="Kwadratura Newtona-Cotesa: ", font=('Arial', 18))
-        # self.label.pack(pady=10)
-
-        # self.text4 = tk.Text(self.root, height=2, font=('Arial', 12))
-        # self.text4.config(state="disabled")
-        # self.text4.pack()
-
-        # self.button5 = tk.Button(self.root, text="Oblicz całke", command=self.simpsonMethod)
-        # self.button5.pack()
 
         self.button_authors = tk.Button(self.root, text="Autorzy", command=self.openPopup)
         self.button_authors.place(relx=0.95, rely=0.95, anchor="se")
@@ -75,9 +65,6 @@ class GUI:
         self.text5 = tk.Text(self.root, height=2, font=('Arial', 12))
         self.text5.config(state="disabled")
         self.text5.pack()
-
-        # self.button6 = tk.Button(self.root, text="Oblicz błąd aproksymacji", command=self.approximationError)
-        # self.button6.pack()
 
         self.button2 = tk.Button(self.root, text="Wygeneruj wykres", command=lambda:self.plot_approximation(approximationRange[0],approximationRange[1],degree))
         self.button2.pack()
@@ -110,18 +97,13 @@ class GUI:
         degree = int(degreeAsString)
         return degree
 
-    # def getEpsilon(self):
-    #     global epsilon
-    #     user_input = self.text6.get("1.0", tk.END)
-    #     epsilonAsString = user_input
-    #     epsilon = float(epsilonAsString)
-    #     return epsilon
+    def getNodes(self):
+        global nodes
+        user_input = self.text4.get("1.0",tk.END)
+        nodes = int(user_input)
+        return nodes
 
-    def dupa(self,x):
-        return x*x
     def openPopup(self):
-        print((2/np.pi) * self.integrate_gauss(lambda t: self.dupa(t) * Tk(2, t) * (1 / np.sqrt(1 - t * t)), -1, 1))
-        print(self.integrate_gauss(lambda t: self.dupa(t), -1, 1))
         popup = tk.Toplevel(self.root, )
         popup.title("Autorzy")
         popup.geometry("400x200")
@@ -138,60 +120,15 @@ class GUI:
 
         approximation = [self.aproximation(i, N) for i in x]
 
-        plt.plot(x, y, label='Original Function')
-        plt.plot(x, approximation, label='Approximation')
+        plt.plot(x, y, label='Funkcja')
+        plt.plot(x, approximation, label='Aproksymacja')
         plt.legend()
         plt.xlabel('x')
-        plt.ylabel('f(x)')
-        plt.title('Chebyshev Polynomial Approximation')
+        plt.ylabel('y')
+        plt.title('Aproksymacja Wielomianami Chebycheva')
         plt.show()
 
-    # def simpsonMethod(self,function):
-    #     n = 1000000
-    #
-    #     # Calculate the step size
-    #     h = 2 / n
-    #
-    #     # Initialize the sum variables
-    #     sum_odd = 0
-    #     sum_even = 0
-    #
-    #     # Perform the summation for odd and even terms
-    #     for i in range(1, n, 2):
-    #         sum_odd += function(-1 + i * h)
-    #
-    #     for i in range(2, n, 2):
-    #         sum_even += function(-1 + i * h)
-    #
-    #     # Calculate the integral value using Simpson's rule
-    #     integral_value = (h / 3) * (function(-0.9999999999999999) + 4 * sum_odd + 2 * sum_even + function(0.9999999999999999))
-    #
-    #     return integral_value
-
-    # def simpsonMethod(self,function):
-    #     n = 100
-    #     print(function(-1))
-    #     result = 1
-    #     s = 0
-    #     a = 0
-    #     # while n > 200:
-    #     while math.fabs(result - s) > 0.00001:
-    #         a += 1
-    #         result = s
-    #         st = 0
-    #         dx = (integralRange[1] - integralRange[0]) / n
-    #
-    #         for i in range(1, n + 1):
-    #             x = integralRange[0] + i * dx
-    #             st += function(x - dx / 2)
-    #             if i < n:
-    #                 s += function(x)
-    #         s = dx / 6 * (function(function(-0.9999999999999999)) + function(function(-0.9999999999999999)) + 2 * s + 4 * st)
-    #         # s = dx / 6 * (function(integralRange[0]) + function(integralRange[1]) + 2 * s + 4 * st)
-    #         n = n * 2
-    #     return s
-
-    def integrate_gauss(self, f,n=5):
+    def integrate_gauss(self, f,n):
         result = 0
         weight = math.pi / n
         for i in range(1, n+1):
@@ -200,9 +137,7 @@ class GUI:
         return weight * result
 
     def coefficient(self,k):
-        # result, _ = scipy.integrate.quad(lambda t: self.calculateFunction(t) * Tk(k, t) * (1 / np.sqrt(1 - t * t)), -1, 1)
-        result = self.integrate_gauss(lambda t: self.calculateFunction(t) * Tk(k, t) * (1 / np.sqrt(1 - t * t)))
-        print(result)
+        result = self.integrate_gauss(lambda t: self.calculateFunction(t) * Tk(k, t) * (1 / np.sqrt(1 - t * t)),nodes)
         return result * (2 / np.pi)
 
 
